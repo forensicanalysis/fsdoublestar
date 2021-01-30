a202638f7b8a82b27514465736c6631e37e8c743
Jonas Plum <git@jonasplum.de>
2021-01-30 15:24:10 +0100

    Add license

--- doublestar_test.go
+++ doublestar_test.go
@@ -1,5 +1,6 @@
 // Copyright (c) 2014-2019 Bob Matcuk
 // Copyright (c) 2019-2020 Siemens AG
+// Copyright (c) 2021 Jonas Plum
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
